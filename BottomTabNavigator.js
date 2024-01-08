22f023779f1b89fe719a90d98fb3288b80affbc7
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-08 09:39:08 +0700

    react native icon working

--- BottomTabNavigator.js
+++ BottomTabNavigator.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen';
 import MarketScreen from './src/screens/MarketScreen';
 import EducationScreen from './src/screens/EducationScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Corrected import
 
 const Tab = createBottomTabNavigator();
 
@@ -12,19 +13,55 @@ const BottomTabNavigator = () => {
     <Tab.Navigator
       screenOptions={{
         tabBarStyle: {
-          backgroundColor: '#00112B', // Ganti warna latar belakang di sini
+          backgroundColor: '#00112B',
           height: 70,
-          borderTopWidth: 0, // Menghapus border di sini
+          borderTopWidth: 0,
         },
         tabBarLabelStyle: {
-          marginBottom: 10, // Ganti posisi teks di sini
+          marginBottom: 10,
         },
-      }}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Market" component={MarketScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Education" component={EducationScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+        tabBarActiveTintColor: 'white',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Market"
+        component={MarketScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="line-chart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Education"
+        component={EducationScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="book" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
