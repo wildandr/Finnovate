07e90d6ef421d9a13c614bcdb2615c9e0cea3392
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-08 09:55:25 +0700

    feat(social media) : icon

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { View, Text, Image, TextInput } from 'react-native';
+import {View, Text, Image, TextInput} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const HomeScreen = () => {
   return (
-    <View style={[tw`flex-1 justify-center items-center`, { backgroundColor: '#002351' }]}>
-      <Image
-        source={require('../assets/Finnovate_logo_top.png')}
-        style={tw`absolute top-5 left-5`}
-      />
-      <TextInput
-        style={[tw`h-10 w-4/5 absolute top-20 rounded-lg px-2`, { backgroundColor: '#00112B', color: 'white', paddingLeft: 20 }]}
-        placeholder=" Search feeds, trend"
-        placeholderTextColor="white"
-      />
+    <View
+      style={[
+        tw`flex-1 justify-center items-center`,
+        {backgroundColor: '#002351'},
+      ]}>
+      <View
+        style={tw`flex-row justify-between items-center absolute w-full px-5 top-5`}>
+        <Image source={require('../assets/Finnovate_logo_top.png')} />
+        <View style={tw`flex-row`}>
+          <Icon name="bell-o" size={24} color="white" style={tw`mr-3`} />
+          <AntDesign name="setting" size={24} color="white" />
+        </View>
+      </View>
+      <View
+        style={[
+          tw`absolute top-20 rounded-lg px-2 flex-row items-center mx-5`,
+          {backgroundColor: '#00112B'},
+        ]}>
+        <Icon name="search" size={20} color="white" style={tw`left-1`} />
+        <TextInput
+          style={[tw`flex-1`, {color: 'white', paddingLeft: 20}]}
+          placeholder=" Search feeds, trend"
+          placeholderTextColor="white"
+        />
+      </View>
       <Text style={tw`text-white`}>Home Screen</Text>
     </View>
   );
